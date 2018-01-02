3806abd892c89edc92ac65375b1ec26ee35177d6
Mark Newton <marknewton@Marks-MacBook-Pro.local>
2018-01-02 12:30:40 -0800

    First commit.

---
+++ index.js
@@ -0,0 +1,24 @@
+module.exports = {
+  "extends": "airbnb",
+  "rules": {
+    "prop-types": 0,
+    "react/jsx-filename-extension": ["error", { "extensions": [".js", ".jsx"] }],
+    "global-require": 0,
+    "strict": 0,
+    "no-console": 1,
+    "object-curly-newline": ["error", { "consistent": true }],
+    "react/prefer-stateless-function": 0,
+    "comma-dangle": ["error", "never"],          // do not allow dangling commas.
+    "no-use-before-define": 0,                   // allow using variables before they are defined.
+    "arrow-body-style": 0,                       // allow curly braces with single line arrow functions.
+    "object-shorthand": 0,                       // allow same name key/value syntax: e.g. { username: username }
+    "padded-blocks": 0,                          // allow a space after a block declaration.
+    "no-underscore-dangle": 0,                   // allow private methods/properties to start with an underscore.
+    "import/prefer-default-export": 0,           // allow single non-default export.
+    "camelcase": 0                               // allow non-camelcase variables/properties.
+  },
+  "globals": {
+    "fetch": false
+  },
+  "parser": "babel-eslint"
+};
