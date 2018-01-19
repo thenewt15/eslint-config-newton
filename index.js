6629d618a9c0a79710a9cde32c3c60ed0f323be6
Mark Newton <marknewton@Marks-MacBook-Pro.local>
2018-01-19 10:42:03 -0800

    Several rule changes.

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ module.exports = {
   "extends": "airbnb",
   "rules": {
     "prop-types": 0,
+    "react/forbid-prop-types": ["error", { "forbid": ["any", "array"] }],
     "react/jsx-filename-extension": ["error", { "extensions": [".js", ".jsx"] }],
     "global-require": 0,
     "strict": 0,
     "no-console": 1,
+    "react/no-unused-state": 1,
+    "react/no-unused-prop-types": 1,
+    "no-unused-vars": 1,
+    "no-trailing-spaces": 0,
     "object-curly-newline": ["error", { "consistent": true }],
+    "function-paren-newline": ["error", "consistent"],
+    "max-len": [2, 120, 2, { "ignoreUrls": true, "ignoreComments": false }],
     "react/prefer-stateless-function": 0,
+    "prefer-destructuring": 0,
     "comma-dangle": ["error", "never"],          // do not allow dangling commas.
     "no-use-before-define": 0,                   // allow using variables before they are defined.
     "arrow-body-style": 0,                       // allow curly braces with single line arrow functions.
